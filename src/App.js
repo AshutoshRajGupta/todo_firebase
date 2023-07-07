d95bab6290bd12ebdba841e8c4814e9b36b58e8b
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-07-07 16:03:27 +0530

    filtering complete and incomplete todos updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+ 
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { initializeApp } from "firebase/app";
@@ -20,62 +20,104 @@ const firebaseConfig = {
   measurementId: "G-8NY3JNC11S"
 };
 
+
 initializeApp(firebaseConfig);
 const auth = getAuth();
 
-// const Navbar = ({ theme, toggleTheme, user, handleLogout, handleLogin }) => {
-//   return (
-//     <nav className="navbar">
-//       <h1 className="navbar-title">Todo App</h1>
-//       {user ? (
-//         <div className="user-info">
-//           <span>Welcome, {user.displayName}</span>
-//           <button className="logout-button" onClick={handleLogout}>
-//             Logout
-//           </button>
-//         </div>
-//       ) : (
-//         <button className="login-button" onClick={handleLogin}>
-//           Login
-//         </button>
-//       )}
-//       <button className="toggle-theme" onClick={toggleTheme}>
-//         {theme === "light" ? "🌙" : "☀️"}
-//       </button>
-//     </nav>
-//   );
-// };
-
-const Navbar = ({ theme, toggleTheme, user, handleLogout, handleLogin }) => {
+const Navbar = ({
+  theme,
+  toggleTheme,
+  user,
+  handleLogout,
+  handleLogin,
+  handleFilterChange,
+  filter,
+}) => {
   return (
-    <nav className="navbar" style={{ background: "blue", padding: "10px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-      <h1 className="navbar-title" style={{ margin: 0, color: "yellow" }}>Todo App</h1>
+    <nav
+      className="navbar"
+      style={{
+        background: "blue",
+        padding: "10px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <h1 className="navbar-title" style={{ margin: 0, color: "yellow" }}>
+        Todo App
+      </h1>
       {user ? (
         <div className="user-info">
-          <span style={{ marginRight: "10px", color: "white" }}>Welcome, {user.displayName}</span>
-          <button className="logout-button" style={{ padding: "5px 10px", background: "darkblue", color: "white", border: "none", cursor: "pointer", transition: "background-color 0.3s ease" }} onClick={handleLogout}>
+          {/* <span style={{ marginRight: "10px", color: "white" }}>
+            Welcome, {user.displayName}
+          </span> */}
+          <span className="username">Welcome, {user.displayName}</span>
+
+          <button
+            className="logout-button"
+            style={{
+              padding: "5px 10px",
+              background: "darkblue",
+              color: "white",
+              border: "none",
+              cursor: "pointer",
+              transition: "background-color 0.3s ease",
+            }}
+            onClick={handleLogout}
+          >
             Logout
           </button>
         </div>
       ) : (
-        <button className="login-button" style={{ padding: "5px 10px", background: "red",margin:"8px 5px 9px 90px", color: "white", border: "none", cursor: "pointer", transition: "background-color 0.3s ease" }} onClick={handleLogin}>
+        <button
+          className="login-button"
+          style={{
+            padding: "5px 10px",
+            background: "red",
+            margin: "8px 5px 9px 90px",
+            color: "white",
+            border: "none",
+            cursor: "pointer",
+            transition: "background-color 0.3s ease",
+          }}
+          onClick={handleLogin}
+        >
           Login
         </button>
       )}
-      <button className="toggle-theme" style={{ padding: "5px",margin:"5px", color: "white", background: "darkblue", border: "none", cursor: "pointer", transition: "transform 0.3s ease" }} onClick={toggleTheme}>
+      <div className="filter-dropdown">
+        <select value={filter} onChange={handleFilterChange}>
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="incomplete">Incomplete</option>
+        </select>
+      </div>
+      <button
+        className="toggle-theme"
+        style={{
+          padding: "5px",
+          margin: "5px",
+          color: "white",
+          background: "darkblue",
+          border: "none",
+          cursor: "pointer",
+          transition: "transform 0.3s ease",
+        }}
+        onClick={toggleTheme}
+      >
         {theme === "light" ? "🌙" : "☀️"}
       </button>
     </nav>
   );
 };
 
-
-
 const App = () => {
   const [theme, setTheme] = useState("light");
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [user, setUser] = useState(null);
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     const storedTodos = localStorage.getItem("todos");
@@ -116,6 +158,7 @@ const App = () => {
       text: inputValue,
       completed: false,
       date: new Date().toLocaleDateString(),
+      canEdit: true,
     };
     setTodos((prevTodos) => [...prevTodos, newTodo]);
     setInputValue("");
@@ -124,7 +167,13 @@ const App = () => {
   const handleToggleComplete = (id) => {
     setTodos((prevTodos) =>
       prevTodos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        todo.id === id
+          ? {
+              ...todo,
+              completed: !todo.completed,
+              canEdit: !todo.completed,
+            }
+          : todo
       )
     );
   };
@@ -162,6 +211,17 @@ const App = () => {
       });
   };
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredTodos =
+    filter === "completed"
+      ? todos.filter((todo) => todo.completed)
+      : filter === "incomplete"
+      ? todos.filter((todo) => !todo.completed)
+      : todos;
+
   return (
     <div className={`app ${theme}`}>
       <Navbar
@@ -170,6 +230,8 @@ const App = () => {
         user={user}
         handleLogout={handleLogout}
         handleLogin={handleLogin}
+        handleFilterChange={handleFilterChange}
+        filter={filter}
       />
       <div className="headline">
         <h2 className="headline-title">Welcome to the Todo App</h2>
@@ -177,52 +239,63 @@ const App = () => {
           Get organized and stay productive with our simple todo list!
         </p>
       </div>
-      <div className="todo-list">
-        {todos.map((todo) => (
-          <div
-            className={`todo ${todo.completed ? "completed" : ""}`}
-            key={todo.id}
-          >
-            <div
-              className="todo-text"
-              onClick={() => handleToggleComplete(todo.id)}
-            >
-              {todo.text}
-            </div>
-            <div className="todo-date">{todo.date}</div>
-            <div className="todo-actions">
-              <button
-                className="edit-button"
-                onClick={() =>
-                  handleEditTodo(
-                    todo.id,
-                    prompt("Edit the todo", todo.text)
-                  )
-                }
-              >
-                ✏️
-              </button>
-              <button
-                className="delete-button"
-                onClick={() => handleDeleteTodo(todo.id)}
-              >
-                ❌
-              </button>
-            </div>
-          </div>
-        ))}
+      <div className="add-todo">
+        <input
+          type="text"
+          placeholder="Add a new todo"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add</button>
       </div>
-      {user && (
-        <div className="add-todo">
-          <input
-            type="text"
-            placeholder="Add a new todo"
-            value={inputValue}
-            onChange={handleInputChange}
-          />
-          <button onClick={handleAddTodo}>Add</button>
+      <div className="todo-container">
+        <div className="todo-list">
+          {filteredTodos.map((todo) => (
+            <div
+              className={`todo ${todo.completed ? "completed" : ""}`}
+              key={todo.id}
+            >
+              <div
+                className="todo-text"
+                onClick={() => handleToggleComplete(todo.id)}
+              >
+                {todo.text}
+              </div>
+              <div className="todo-date">{todo.date}</div>
+              <div className="todo-actions">
+                {!todo.completed && todo.canEdit && (
+                  <button
+                    className="edit-button"
+                    onClick={() =>
+                      handleEditTodo(
+                        todo.id,
+                        prompt("Edit the todo", todo.text)
+                      )
+                    }
+                  >
+                    ✏️
+                  </button>
+                )}
+                <button
+                  className="delete-button"
+                  onClick={() => handleDeleteTodo(todo.id)}
+                >
+                 ❌
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-      )}
+      </div>
+      {/* <div className="add-todo">
+        <input
+          type="text"
+          placeholder="Add a new todo"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div> */}
     </div>
   );
 };
